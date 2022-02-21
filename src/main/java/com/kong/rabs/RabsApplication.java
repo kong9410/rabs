f04e672c18336fd4d83d80d9bfc87ad24277c39f
kong <kgong1013@gmail.com>
2022-02-21 22:57:58 +0900

    init

---
+++ src/main/java/com/kong/rabs/RabsApplication.java
@@ -0,0 +1,11 @@
+package com.kong.rabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kong/rabs/RabsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kong.rabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
