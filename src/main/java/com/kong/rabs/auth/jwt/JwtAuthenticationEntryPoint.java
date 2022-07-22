669bdb177e1583cbf5cf8404358ee9c895c772f8
kong <kgong1013@gmail.com>
2022-07-22 22:40:03 +0900

    회원가입 로그인 개발
    https://github.com/kong9410/rabs/issues/1

--- src/main/java/com/kong/rabs/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/kong/rabs/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package com.kong.rabs.auth.jwt;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +15,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         log.error("Responding with unauthorized error. Message - {}", authException.getMessage());
 
-        response.sendError(HttpServletResponse.SC_ACCEPTED);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
